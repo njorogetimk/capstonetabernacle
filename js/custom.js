a248584a60cb7c49ac8cfd4f2e8154ecd2957b9d
Timothy Njoroge <njorogetimk@gmail.com>
2020-02-16 19:53:04 +0300

    add changes made by prettier

--- js/custom.js
+++ js/custom.js
@@ -7,65 +7,65 @@ var root2 = document.querySelector(".breadcrumb");
 // console.log(root2);
 
 switch (domain) {
-    case "njorogetimk.github.io":
-        newRoot = "/website";
-        root.href = newRoot;
-        if (root2 != null) {
-            root2.href = newRoot;
-        }
-        break;
-    default:
-        break;
+  case "njorogetimk.github.io":
+    newRoot = "/website";
+    root.href = newRoot;
+    if (root2 != null) {
+      root2.href = newRoot;
+    }
+    break;
+  default:
+    break;
 }
 
 // Opera Mini Browser detection
 const isOperaMini = navigator.userAgent.indexOf("Opera Mini") > -1;
 
 if (isOperaMini) {
-    let opera = document.querySelector("#opera");
-    opera.innerHTML =
-        '<p> Please enable <em style = "font-weight:bold"> JavaScript</em ></p>  <p>For <em style="font-weight:bold">Opera Mini</em> users click <a href="opera.html">here</a> for how disable JavaScript</p> <h6 class="center"><a class="red white-text btn" href="opera.html">For Opera Mini Users</a></h6>';
+  let opera = document.querySelector("#opera");
+  opera.innerHTML =
+    '<p> Please enable <em style = "font-weight:bold"> JavaScript</em ></p>  <p>For <em style="font-weight:bold">Opera Mini</em> users click <a href="opera.html">here</a> for how disable JavaScript</p> <h6 class="center"><a class="red white-text btn" href="opera.html">For Opera Mini Users</a></h6>';
 } else {
-    let opera = document.querySelector("#opera");
-    if (opera) {
-        opera.style.display = "none";
-    }
+  let opera = document.querySelector("#opera");
+  if (opera) {
+    opera.style.display = "none";
+  }
 }
 
 // navigation Bar
 document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll(".sidenav");
-    var instances = M.Sidenav.init(elems, { preventScrolling: true });
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, { preventScrolling: true });
 });
 // Collapsible menus in sidenav
 document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll(".collapsible");
-    var instances = M.Collapsible.init(elems, {});
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {});
 });
 // Slider
 // Slider
 document.addEventListener("DOMContentLoaded", function() {
-    var slider = document.querySelectorAll(".slider");
-    var instances = M.Slider.init(slider, {});
+  var slider = document.querySelectorAll(".slider");
+  var instances = M.Slider.init(slider, {});
 });
 
 // material boxed
 document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll(".materialboxed");
-    var instances = M.Materialbox.init(elems, {});
+  var elems = document.querySelectorAll(".materialboxed");
+  var instances = M.Materialbox.init(elems, {});
 });
 
 // Collapsible menus in sidenav
 document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll(".newCollapsible");
-    var instances = M.Collapsible.init(elems, {});
+  var elems = document.querySelectorAll(".newCollapsible");
+  var instances = M.Collapsible.init(elems, {});
 });
 
 // Dropdown
 document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll(".dropdown-trigger");
-    var instances = M.Dropdown.init(elems, {
-        constrainWidth: false,
-        coverTrigger: false
-    });
+  var elems = document.querySelectorAll(".dropdown-trigger");
+  var instances = M.Dropdown.init(elems, {
+    constrainWidth: false,
+    coverTrigger: false
+  });
 });
